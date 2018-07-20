f6af48563c7b681108fa37fcf14963df9537c199
Jakub Sobel <qbasob@gmail.com>
2018-07-20 12:55:52 +0200

    testowy endpoint 409: Conflict;

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const jsonServer = require('json-server');
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
+server.use(cors({ origin: 'http://localhost:8100' }));
 server.use(function (req, res, next) {
     setTimeout(next, 500); // delay w responsach
 });
+
+server.patch('/people/1', (req, res) => {
+    res.status(409).json({
+        message: "Błąd zapisu. Konflikt danych."
+    });
+});
+
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
